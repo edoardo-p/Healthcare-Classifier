5e92e34d5b3294e41fbdc4fae97c79eb98ed2192
edoardo-p <edoardo.protti@gmail.com>
2023-03-30 09:10:37 +0200

    Added RF classifier

--- rf.py
+++ rf.py
@@ -5,31 +5,9 @@ import pandas as pd
 from joblib import dump
 from sklearn.base import clone
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 from scores import print_metrics
-
-
-def cross_validation(
-    estimator, X: pd.DataFrame, y: pd.DataFrame, neg: int, k=5, seed=42
-) -> list:
-    np.random.seed(seed)
-    idxs = X.index.values
-    np.random.shuffle(idxs)
-    models = [clone(estimator) for _ in range(k)]
-    for i in range(k):
-        start = i * len(X) // k
-        end = (i + 1) * len(X) // k
-        test_fold = idxs[start:end]
-        train_fold = np.setdiff1d(idxs, test_fold)
-        # X_test = X.loc[test_fold]
-        # y_test = y.loc[test_fold]
-        X_train = X.loc[train_fold]
-        y_train = y.loc[train_fold]
-        models[i].fit(X_train, y_train)
-        dump(models[i], f".\\models\\svc\\svc_1v{neg}_fold{i}.joblib")
-
-    return models
 
 
 def pca(X: pd.DataFrame, threshold: float = 0.9, savefile: str = None) -> pd.DataFrame:
@@ -52,7 +30,7 @@ def reduce(X: pd.DataFrame, components: pd.DataFrame) -> pd.DataFrame:
     return X @ components
 
 
-def svm_classification(
+def rf_classification(
     df: pd.DataFrame, labels: pd.Series, neg_class: int, **kwargs
 ) -> None:
     seed = kwargs.get("seed", 42)
@@ -71,9 +49,9 @@ def svm_classification(
         X_train = (X_train - X_train.mean(axis=0)) / X_train.std(axis=0)
         X_test = (X_test - X_test.mean(axis=0)) / X_test.std(axis=0)
 
-    # Training k-folds CV
-    svc = SVC(kernel="linear", random_state=seed, C=5)
-    svc_models = cross_validation(svc, X_train, y_train, neg_class, k=k, seed=seed)
-    predictions = np.array([model.predict(X_test) for model in svc_models])
-    y_pred = np.count_nonzero(predictions, axis=0) > (k // 2)
-    print_metrics(y_test, y_pred, f"SVC 1v{neg_class}")
+    # Training RF
+    rf = RandomForestClassifier(max_depth=2, random_state=seed)
+    rf.fit(X_train, y_train)
+    dump(rf, f".\\models\\rf\\rf_1v{neg_class}.joblib")
+    y_pred = rf.predict(X_test)
+    print_metrics(y_test, y_pred, f"RF 1v{neg_class}")
