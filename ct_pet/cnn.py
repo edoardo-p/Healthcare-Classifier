3c00bf08663174a17278937625245a1ca7710289
edoardo-p <61146513+edoardo-p@users.noreply.github.com>
2023-05-12 18:51:55 +0200

    CNN

--- ct_pet/cnn.py
+++ ct_pet/cnn.py
@@ -1,42 +1,34 @@
 import tensorflow as tf
 
 
-class Conv(tf.keras.Model):
+class CTConv(tf.keras.Model):
     def __init__(self):
         super().__init__()
-        self.conv1 = tf.keras.layers.Conv3D(32, 3, activation="relu")
-        self.conv2 = tf.keras.layers.Conv3D(64, 3, activation="relu")
-        self.conv3 = tf.keras.layers.Conv3D(128, 3, activation="relu")
+        self.conv1 = tf.keras.layers.Conv3D(32, 16, activation="relu")
+        self.conv2 = tf.keras.layers.Conv3D(64, 16, activation="relu")
+        self.conv3 = tf.keras.layers.Conv3D(128, 16, activation="relu")
         self.pooling = tf.keras.layers.MaxPool3D()
 
         self.flatten = tf.keras.layers.Flatten()
-        self.dense1 = tf.keras.layers.Dense(128, activation="relu")
+        # self.dense1 = tf.keras.layers.Dense(128, activation="relu")
         self.dense2 = tf.keras.layers.Dense(64, activation="relu")
-        self.dense3 = tf.keras.layers.Dense(10)
+        self.dense3 = tf.keras.layers.Dense(1, activation="sigmoid")
 
     def call(self, x):
-        _, w, h, _, _ = x.shape
-        if w != h or w not in (128, 256, 512):
-            raise ValueError(
-                "Input tensor must be square of sizes (128x128), (256x256) or (512x512)"
-            )
-
-        pooling = (w // 128, w // 128, 1)
-        x = tf.keras.layers.MaxPool3D(pooling, strides=pooling)(x)
-
         x = self.conv1(x)
         x = self.pooling(x)
         x = self.conv2(x)
         x = self.pooling(x)
         x = self.conv3(x)
         x = self.pooling(x)
+
         x = self.flatten(x)
-        x = self.dense1(x)
+        # x = self.dense1(x)
         x = self.dense2(x)
         return self.dense3(x)
 
 
 if __name__ == "__main__":
-    model = Conv()
-    model.build((None, 128, 128, 1, 1))
+    model = CTConv()
+    model.build((None, 128, 512, 512, 1))
     model.summary()
