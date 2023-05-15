73d2474fc110c4fde74b2dbec1c529d213a3e8b1
edoardo-p <edoardo.protti@gmail.com>
2023-05-15 14:12:59 +0200

    typed class

--- ct_pet/data.py
+++ ct_pet/data.py
@@ -1,17 +1,18 @@
+from typing import Iterable, SupportsFloat
 import torch
 
 
 class CTData(torch.utils.data.Dataset):
-    def __init__(self, images, masks):
+    def __init__(self, images: Iterable[SupportsFloat], masks: Iterable[SupportsFloat]):
         super().__init__()
-        self.images = images
-        self.masks = masks
+        self.data = list(zip(images, masks))
 
-    def __len__(self):
-        return self.images.shape[0]
+    def __len__(self) -> int:
+        return len(self.data)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> tuple[torch.Tensor, torch.Tensor]:
+        image, mask = self.data[idx]
         return (
-            torch.tensor(self.images[idx]).unsqueeze(0),
-            torch.tensor(self.masks[idx]).unsqueeze(0),
+            torch.tensor(image, dtype=torch.float).unsqueeze(0),
+            torch.tensor(mask, dtype=torch.float).unsqueeze(0),
         )
