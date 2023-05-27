9644bd30182fdde0dc1711cd21a2eb9b1ddaece5
edoardo-p <61146513+edoardo-p@users.noreply.github.com>
2023-05-27 20:13:25 +0200

    Removed sidebar, improved cropper

--- ct_pet/main.py
+++ ct_pet/main.py
@@ -1,86 +1,100 @@
 import numpy as np
-import SimpleITK as sitk
 import streamlit as st
+from bounding_box import BoundingBox
 from PIL import Image
 from streamlit_cropper import st_cropper
 
-from bounding_box import BoundingBox
 
-bb = BoundingBox(64)
+@st.cache_resource
+def load_image(image_file):
+    return np.rot90(np.load(image_file), 2, (1, 2))
+
+
+# from streamlit_modal import Modal
+def get_box(*args, **kwargs):
+    return {
+        "top": st.session_state.get("top", 224),
+        "left": st.session_state.get("left", 224),
+        "width": 64,
+        "height": 64,
+    }
 
 
 def main():
-    # st.cache(suppress_st_warning=True)
-    st.set_page_config(layout="wide")
-    app_mode = st.sidebar.radio(
-        "Select Page", ["Home Page", "Prediction", "privacy policy"]
-    )
-    uploaded_data = None
+    # modal = Modal("Data and Security", "privacy_policy_modal")
+    modal = None
+    st.set_page_config(layout="centered")
 
-    if app_mode == "Home Page":
+    if "page" not in st.session_state:
+        st.session_state["page"] = "upload"
+
+    if st.session_state["page"] == "login":
+        st.title("Login")
+        st.text_input("Username")
+        st.text_input("Password", type="password")
+        open_modal = st.button(label="button")
+        if open_modal:
+            with modal.container():
+                st.write("Personal data shall be:")
+                st.markdown(
+                    """
+                    1. processed lawfully, fairly and in a transparent manner in relation to individuals (lawfulness, fairness and transparency)
+                    1. collected for specified, explicit and legitimate purposes and not further processed in a manner that is incompatible with those purposes; further processing for archiving purposes in the public interest, scientific or historical research purposes or statistical purposes shall not be considered to be incompatible with the initial purposes (purpose limitation)
+                    1. adequate, relevant and limited to what is necessary in relation to the purposes for which they are processed (data minimisation)
+                    1. accurate and, where necessary, kept up to date; every reasonable step must be taken to ensure that personal data that are inaccurate, having regard to the purposes for which they are processed, are erased or rectified without delay (accuracy)
+                    1. kept in a form which permits identification of data subjects for no longer than is necessary for the purposes for which the personal data are processed; personal data may be stored for longer periods insofar as the personal data will be processed solely for archiving purposes in the public interest, scientific or historical research purposes or statistical purposes subject to implementation of the appropriate technical and organisational measures required by the GDPR in order to safeguard the rights and freedoms of individuals (storage limitation)
+                    1. processed in a manner that ensures appropriate security of the personal data, including protection against unauthorised or unlawful processing and against accidental loss, destruction or damage, using appropriate technical or organisational measures (integrity and confidentiality).
+                    """
+                )
+        if st.checkbox("Agree to the privacy policy") and st.button("Login"):
+            st.session_state["page"] = "upload"
+            st.experimental_rerun()
+
+    # app_mode = st.sidebar.radio(
+    #     "Select Page", ["Home Page", "Prediction", "privacy policy"]
+    # )
+
+    if st.session_state["page"] == "upload":
         st.title("Radiogram")
-        img_file = st.file_uploader(
-            "Upload the CT scan",
-            type=["png", "jpg", "npy", "nii"],
-            accept_multiple_files=False,
-        )
-        st.set_option("deprecation.showfileUploaderEncoding", False)
+        image_file = st.file_uploader("Upload the CT scan", type="npy")
 
-        # Upload an image and set some options for demo purposes
-        st.header("Region of interest")
-        box_color = st.color_picker(label="Box Color", value="#0000FF")
-
-        if img_file:
-            ct_scan = np.load(img_file)
+        if image_file:
+            st.header("Select region of interest")
+            ct_scan = load_image(image_file)
             if "slice" not in st.session_state:
-                st.session_state["slice"] = 159
+                st.session_state["slice"] = ct_scan.shape[0] // 2
             slc = st.session_state["slice"]
-            # ct_scan = sitk.GetArrayFromImage(sitk.ReadImage(img_file))
-            # Get a cropped image from the frontend
+
             cropped = st_cropper(
                 Image.fromarray(ct_scan[slc, :, :]),
-                box_color=box_color,
                 aspect_ratio=(1, 1),
-                box_algorithm=bb.get_box,
+                box_algorithm=get_box,
+                return_type="box",
             )
-            st.write(st.session_state["slice"])
-            if st.button("<-"):
-                st.session_state["slice"] = slc - 1
 
-            if st.button("->"):
-                st.session_state["slice"] = slc + 1
+            buttons = {
+                r"$\triangleleft\triangleleft$": -5,
+                r"$\triangleleft$": -1,
+                r"$\triangleright$": 1,
+                r"$\triangleright\triangleright$": 5,
+            }
+            for button, (icon, offset) in zip(st.columns(4), buttons.items()):
+                with button:
+                    if st.button(icon, key=f"button_{icon}"):
+                        st.session_state["slice"] = slc + offset
+                        st.experimental_rerun()
 
-            # Manipulate cropped image at will
-            # cropped_img1 = Image.open(cropped_img1)
-            # st.write("Preview")
-            # cropped.thumbnail((150, 150))
-            # st.image(cropped)
+            st.write("Preview")
+            st.image(
+                ct_scan[
+                    slc,
+                    cropped["top"] : cropped["top"] + cropped["height"],
+                    cropped["left"] : cropped["left"] + cropped["width"],
+                ],
+                clamp=True,
+            )
 
-    if app_mode == "privacy policy":
-        st.title("Data and Security")
-        st.write("Personal data shall be:")
-        st.write(
-            "(a) processed lawfully, fairly and in a transparent manner in relation to individuals  (lawfulness, fairness and transparency)"
-        )
-        st.write(
-            "(b) collected for specified, explicit and legitimate purposes and not further processed in a manner that is incompatible with those purposes; further processing for archiving purposes in the public interest, scientific or historical research purposes or statistical purposes shall not be considered to be incompatible with the initial purposes (purpose limitation)"
-        )
-        st.write(
-            "(c) adequate, relevant and limited to what is necessary in relation to the purposes for which they are processed (data minimisation)"
-        )
-        st.write(
-            "(d) accurate and, where necessary, kept up to date; every reasonable step must be taken to ensure that personal data that are inaccurate, having regard to the purposes for which they are processed, are erased or rectified without delay (accuracy)"
-        )
-        st.write(
-            "(e) kept in a form which permits identification of data subjects for no longer than is necessary for the purposes for which the personal data are processed; personal data may be stored for longer periods insofar as the personal data will be processed solely for archiving purposes in the public interest, scientific or historical research purposes or statistical purposes subject to implementation of the appropriate technical and organisational measures required by the GDPR in order to safeguard the rights and freedoms of individuals (storage limitation)"
-        )
-        st.write(
-            "(f) processed in a manner that ensures appropriate security of the personal data, including protection against unauthorised or unlawful processing and against accidental loss, destruction or damage, using appropriate technical or organisational measures (integrity and confidentiality)."
-        )
-        st.checkbox("I agree")
-        # st.download_button("confirm", img_ct)
-
-    if app_mode == "Prediction":
+    if st.session_state["page"] == "Prediction":
         st.title("Result of analysis")
 
 
