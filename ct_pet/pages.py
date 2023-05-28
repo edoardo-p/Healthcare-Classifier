79a1e3205ebd094cdf0eae4617223b8ec1eb993e
edoardo-p <61146513+edoardo-p@users.noreply.github.com>
2023-05-28 11:25:20 +0200

    Separated page content into functions

---
+++ ct_pet/pages.py
@@ -0,0 +1,118 @@
+import numpy as np
+import streamlit as st
+from PIL import Image
+from streamlit_cropper import st_cropper
+
+
+@st.cache_resource
+def load_image(image_file):
+    return np.rot90(np.load(image_file), 2, (1, 2))
+
+
+def get_box(*args, **kwargs):
+    return {
+        "top": st.session_state.get("top", 224),
+        "left": st.session_state.get("left", 224),
+        "width": 64,
+        "height": 64,
+    }
+
+
+def login_page():
+    # modal = Modal("Data and Security", "privacy_policy_modal")
+    modal = None
+    st.title("Login")
+    st.text_input("Username")
+    st.text_input("Password", type="password")
+    open_modal = st.button(label="button")
+    if open_modal:
+        with modal.container():
+            st.write("Personal data shall be:")
+            st.markdown(
+                """
+                1. processed lawfully, fairly and in a transparent manner in relation to individuals (lawfulness, fairness and transparency)
+                1. collected for specified, explicit and legitimate purposes and not further processed in a manner that is incompatible with those purposes; further processing for archiving purposes in the public interest, scientific or historical research purposes or statistical purposes shall not be considered to be incompatible with the initial purposes (purpose limitation)
+                1. adequate, relevant and limited to what is necessary in relation to the purposes for which they are processed (data minimisation)
+                1. accurate and, where necessary, kept up to date; every reasonable step must be taken to ensure that personal data that are inaccurate, having regard to the purposes for which they are processed, are erased or rectified without delay (accuracy)
+                1. kept in a form which permits identification of data subjects for no longer than is necessary for the purposes for which the personal data are processed; personal data may be stored for longer periods insofar as the personal data will be processed solely for archiving purposes in the public interest, scientific or historical research purposes or statistical purposes subject to implementation of the appropriate technical and organisational measures required by the GDPR in order to safeguard the rights and freedoms of individuals (storage limitation)
+                1. processed in a manner that ensures appropriate security of the personal data, including protection against unauthorised or unlawful processing and against accidental loss, destruction or damage, using appropriate technical or organisational measures (integrity and confidentiality).
+                """
+            )
+    if st.checkbox("Agree to the privacy policy") and st.button("Login"):
+        st.session_state["page"] = "upload"
+        st.experimental_rerun()
+
+
+def upload_page():
+    st.title("Radiogram")
+    image_file = st.file_uploader("Upload the CT scan", type="npy")
+
+    if image_file:
+        st.header("Select region of interest")
+        ct_scan = load_image(image_file)
+        if "slice" not in st.session_state:
+            st.session_state["slice"] = ct_scan.shape[0] // 2
+        slc = st.session_state["slice"]
+
+        cropped: dict[str, int] = st_cropper(
+            Image.fromarray(ct_scan[slc, :, :]),
+            aspect_ratio=(1, 1),
+            box_algorithm=get_box,
+            return_type="box",
+        )
+
+        buttons = {
+            r"$\triangleleft\triangleleft$": -5,
+            r"$\triangleleft$": -1,
+            r"$\triangleright$": 1,
+            r"$\triangleright\triangleright$": 5,
+        }
+        for button, (icon, offset) in zip(st.columns(4), buttons.items()):
+            with button:
+                if st.button(icon, key=f"button_{icon}"):
+                    st.session_state["slice"] = slc + offset
+                    st.experimental_rerun()
+
+        st.write("Preview")
+        st.image(
+            ct_scan[
+                slc,
+                cropped["top"] : cropped["top"] + cropped["height"],
+                cropped["left"] : cropped["left"] + cropped["width"],
+            ],
+            clamp=True,
+        )
+
+        if st.button("Find tumor"):
+            st.session_state["page"] = "segmentation"
+            st.session_state["top"] = cropped["top"]
+            st.session_state["left"] = cropped["left"]
+            st.session_state["image_file"] = image_file
+            st.experimental_rerun()
+
+
+def segmentation_page():
+    st.title("Segmented mask")
+    top = st.session_state["top"]
+    left = st.session_state["left"]
+    slc = st.session_state["slice"]
+    ct_scan = load_image(st.session_state["image_file"])
+    original, mask = st.columns(2)
+
+    with original:
+        for i, col in enumerate(st.columns(4)):
+            col.image(ct_scan[slc + i, left : left + 64, top : top + 64], clamp=True)
+            col.image(
+                ct_scan[slc + i + 4, left : left + 64, top : top + 64], clamp=True
+            )
+            col.image(
+                ct_scan[slc + i + 8, left : left + 64, top : top + 64], clamp=True
+            )
+            col.image(
+                ct_scan[slc + i + 12, left : left + 64, top : top + 64], clamp=True
+            )
+
+
+def prediction_page():
+    st.title("Predictions")
+    st.write("TODO")
